6bbd88e393ba25895d51771a74418bc479b972c4
Yrobitaille <yoan.robitaille@hotmail.com>
2026-02-26 14:47:54 -0500

    Fait le init et le selected type de poutine.js

---
+++ src/scripts/Poutine.js
@@ -0,0 +1,22 @@
+export default class Poutine {
+  constructor(element) {
+    this.element = element;
+    this.types = this.element.querySelectorAll('.button');
+    this.selectedType = '';
+    this.init();
+  }
+  init() {
+    for (let i = 0; i < this.types.length; i++) {
+      const type = this.types[i];
+      type.addEventListener('click', this.selectType.bind(this));
+    }
+  }
+  selectType(e) {
+    for (let i = 0; i < this.types.length; i++) {
+      const type = this.types[i];
+      type.classList.remove('is-active');
+    }
+    e.currentTarget.classList.add('is-active');
+    this.selectedType = e.target.innerText;
+  }
+}
